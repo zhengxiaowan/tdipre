36c62ead0effc903376a22ca78005e2fcac49da3
zhengxiaowan <xiaowanzheng@gmail.com>
2017-12-30 23:01:24 -0500

    third push

--- app.py
+++ app.py
@@ -15,17 +15,25 @@ def index():
         return render_template('index.html')
     else:
         name=request.form['name']        
-        selected = request.form['plot']
-        #selected = str(selected)
+        #open_value = request.form['open']
+        #close_value = request.form['close']
+        #high_value = request.form['high']
+        #low_value = request.form['low']
+        selected=request.form['plot']
         dictionary = {'api_key':'XyT1FY3chW_8gD_g4ytW', 'date.lt':'2017-11-30', 'date.gt':'2017-11-01'}
         r = requests.get('https://www.quandl.com/api/v3/datatables/WIKI/PRICES.json', params=dictionary)
         x = r.json()
         df=pd.DataFrame(x['datatable']['data'])
         df.columns = pd.DataFrame(x['datatable']['columns'])['name']
         dfnew=df[['ticker','date','open','high','low','close']]
-        x1=dfnew[dfnew['ticker']==name]['date'][0]
-        y=dfnew[dfnew['ticker']==name][selected][0]            
-        return y
+        x1=dfnew[dfnew['ticker']==name]['date'].values.tolist()
+        y=dfnew[dfnew['ticker']==name][selected].values.tolist()
+        x=[datetime.datetime.strptime(i, '%Y-%m-%d') for i in x1]
+        output_file('result.htm')
+        p = figure(title="Price Graph", x_axis_label='Date', y_axis_label='Price', x_axis_type="datetime")
+        p.line(x, y, line_width=2)
+        show(p)
+        return result.htm
 
 if __name__ == '__main__':
   app.run(port=33507)
